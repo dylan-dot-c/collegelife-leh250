d40a837f91a0653acab1f9a36857197dd11441c6
Dylan Heslop <heslopd23@gmail.com>
2024-10-13 08:50:43 -0400

    hotfix: portable text component now compatible

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { getPostBySlug } from "@/sanity/lib/getPosts";
-import { PortableText } from "@portabletext/react";
+import { PortableText, PortableTextComponents } from "@portabletext/react";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
@@ -7,7 +7,8 @@ import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 export const revalidate = 10;
 
 const Project = async ({ params }: { params: { slug: string } }) => {
-    const PortableTextComponent = {
+    // Define PortableText components with proper types
+    const PortableTextComponent: PortableTextComponents = {
         types: {
             image: ({ value }: { value: SanityImageSource }) => (
                 <Image
@@ -15,30 +16,32 @@ const Project = async ({ params }: { params: { slug: string } }) => {
                     alt={"image"}
                     className='w-full rounded-md max-h-[400px] object-cover'
                     width={800}
-                    height={15}
-                    // height={1000}
+                    height={400} // Adjust height here to fit the image properly
                 />
             ),
         },
         block: {
-            h1: ({ children }: { children: React.ReactNode }) => (
+            h1: ({ children }) => (
                 <h1 className='text-3xl mb-4 font-bold font-mono'>
                     {children}
                 </h1>
             ),
-            h2: ({ children }: { children: React.ReactNode }) => (
-                <h2>{children}</h2>
+            h2: ({ children }) => (
+                <h2 className='text-2xl mb-4 font-bold'>{children}</h2>
             ),
-            // Add more block styles (e.g., h3, blockquote, etc.) as needed
-            normal: ({ children }: { children: React.ReactNode }) => (
-                <p className='my-2'>{children}</p>
+            normal: ({ children }) => (
+                <p className='my-2 text-lg'>{children}</p>
             ),
         },
     };
+
     const project = await getPostBySlug(params.slug);
-    console.log(params.slug);
-    console.log(project);
-    // const post = project[0];
+
+    // Ensure we handle possible undefined states properly
+    if (!project || project.length === 0) {
+        return <div>Project not found</div>;
+    }
+
     return (
         <div className='max-w-3xl mx-auto mt-6 px-4'>
             <h1 className='capitalize mb-4 font-bold font-serif'>
